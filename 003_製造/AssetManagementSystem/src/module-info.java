5d7be030eccf7ee7d58adab80c2ac8dce0ebc41f
83T45B3\User <tesy@gmail.com>
2026-04-06 09:24:31 +0900

    テスト画面の削除

--- 003_製造/AssetManagementSystem/src/module-info.java
+++ 003_製造/AssetManagementSystem/src/module-info.java
@@ -39,7 +39,6 @@ module AssetManagementSystem {
 	opens application.java.window.inventoryLoans.inventoryReturn to javafx.fxml;
 	opens application.java.window.verifyLocation.performInventory to javafx.fxml;
 	opens application.java.window.masterMaintenance.stockTypeMaster to javafx.fxml;
-	opens application.java.window.testNextWindow to javafx.fxml;
 	opens application.resources.xml to org.mybatis;
 	opens application.resources.mapper to org.mybatis;
 }
